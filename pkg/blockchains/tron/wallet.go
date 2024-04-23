46277f53b86a02a947f9d7901f1f1fc3a85bdfce
Ramin Farmani <ramin.farmani@gmail.com>
2024-04-23 22:17:42 +0330

    Resolve STE-22

--- pkg/blockchains/tron/wallet.go
+++ pkg/blockchains/tron/wallet.go
@@ -1,5 +1,10 @@
 package tron
 
+import (
+	"errors"
+	"github.com/ranjbar-dev/tron-wallet/util"
+)
+
 type Wallet struct {
 	privateKey    string
 	publicKey     string
@@ -21,19 +26,27 @@ func (w *Wallet) GetBalance() float64 {
 }
 
 func (w *Wallet) GetAddressBase58() string {
+	if w.AddressBase58 == "" && w.AddressHex != "" {
+		w.AddressBase58, _ = convertBase58ToHex(w.AddressHex)
+	}
+
 	return w.AddressBase58
 }
 
 func (w *Wallet) GetAddressHex() string {
-	if w.AddressHex == "" {
-		return convertBase58ToHex(w.AddressBase58)
+	if w.AddressHex == "" && w.AddressBase58 != "" {
+		w.AddressHex, _ = convertBase58ToHex(w.AddressBase58)
 	}
 
 	return w.AddressHex
 }
 
-func convertBase58ToHex(base58 string) string {
-	return ""
+func convertBase58ToHex(input string) (string, error) {
+	addr, err := util.DecodeCheck(input)
+	if err != nil {
+		return "", err
+	}
+	return string(addr[:]), nil
 }
 
 func NewWallet(privateKey, base58 string) *Wallet {
@@ -42,3 +55,9 @@ func NewWallet(privateKey, base58 string) *Wallet {
 		AddressBase58: base58,
 	}
 }
+
+var (
+	ErrDecodeLength = errors.New("base58 decode length error")
+	ErrDecodeCheck  = errors.New("base58 check failed")
+	ErrEncodeLength = errors.New("base58 encode length error")
+)
